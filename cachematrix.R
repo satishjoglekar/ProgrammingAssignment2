10209901d163bdc1101bf87914348dbbe65cc6c3
satishjoglekar <sjoglekars@gmail.com>
2016-01-22 19:03:09 +0530

    Update cachematrix.R
    The code for matrix inversion as a part of  assignment for R programming week3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,70 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Takes matrix as input and computes it's Inverse
+## Saves the Inverse in cache so it can be reused when required
+## without having to recalculate the inverse each time it is required
 
-## Write a short comment describing this function
+## makeCacheMatrix Function takes the matrix as input function
+## Returns a vector containg  pointers to functions, namely:
+##      get(), set(), setInv() and getInv()
 
 makeCacheMatrix <- function(x = matrix()) {
+
+   # inv is in local in current env
+   inv <- matrix()
+   
+   #sets the matrix data with the matrix argument
+   set <- function(y) {
+    #print("inside set function")
+    # m here is being set in another env 
+    m <<- y 
+    # inv here is being set in another env
+    inv <<- NULL
+    #print("leaving set function")
+    #print(m)
+    }
+    
+  # return the matrix
+  get <- function() {
+    #print("in get function") 
+    m
+  }
+  
+  # sets the inverse
+  setInv <- function(inverse){ 
+    #print("In setInv function")
+    inv <<- inverse
+  }
+  
+  #returns the inverse
+  getInv <- function(){
+    #print("in getInv function") 
+    inv
+  }
+  list(set = set, get = get,
+       setInv = setInv,
+       getInv = getInv)
+  
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve function Computes the Matrix Inverse if not cached
+## Else returns the cached value of the inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  # will return cached or calculated inverse
+  #print("in function cacheSolve")
+ 
+  inv<-x$getInv()
+  
+  if(!is.null(inv)){
+    message("returning cached data")
+    return(inv)
+    
+  }
+  # reading Matrix!! in order to Calculate it's inverse"
+  matrixData<-x$get()
+  inv<-solve(matrixData)
+  x$setInv(inv)
+  inv
 }
